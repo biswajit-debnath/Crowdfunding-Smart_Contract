737e549ee23145611de2996a6f9cf519e54c6de2
biswajit-debnath <biswajitdebnath405@gmail.com>
2025-02-10 13:34:47 +0530

    Feature: Deployer added in the test script

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,29 +3,31 @@
 pragma solidity 0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
 
     FundMe fundMe;
+    address aggregatorV3Address;
 
     function setUp() external {
-        // vm.startBroadcast();
-        // fundMe = new FundMe();
-        // vm.stopBroadcast();
+   
+        DeployFundMe deployer = new DeployFundMe();
+        (fundMe, aggregatorV3Address) = deployer.run();
     }
 
-    // function testMinimumAmountForFunding() public {
-    //     // Arrage
-    //     uint256 minimumAmountForFunding = 5e18;
+    function testMinimumAmountForFunding() public {
+        // Arrage
+        uint256 minimumAmountForFunding = 5e18;
 
-    //     // Act
-    //     uint256 minimumAmountInContract = fundMe.getMinimumAmountForFunding();
-    //     console.log("minimumAmountInContract::", minimumAmountInContract);
-    //     console.log("minimumAmountForFunding::", minimumAmountForFunding);
+        // Act
+        uint256 minimumAmountInContract = fundMe.getMinimumAmountForFunding();
+        console.log("minimumAmountInContract::", minimumAmountInContract);
+        console.log("minimumAmountForFunding::", minimumAmountForFunding);
 
 
-    //     // Assert
-    //     assertEq(minimumAmountForFunding, minimumAmountInContract);
-    // }
+        // Assert
+        assertEq(minimumAmountForFunding, minimumAmountInContract);
+    }
 
 }
